01d875d00f4526d5952349346a0c8615e6189b28
guptaamit5 <141409978+guptaamit5@users.noreply.github.com>
2023-09-08 10:34:55 +0530

    Update 21_3_Amitgupta.c

--- 21_3_Amitgupta.c
+++ 21_3_Amitgupta.c
@@ -5,67 +5,39 @@ SE-IT (sem 3)
 2023-24
 */
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-/*stucture of a student*/
-struct student
-{
-    int rollNo;
-    char name[20];
-    float perc;
-}std[5];
+struct Student {
+    int roll_no;
+    char name[50];
+    float percentage;
+};
 
-void main(){
-    /*taking in 5 student details*/
-    for(int i=0; i<5; i++)
-    {
-        printf("enter the details of %d th student\n", i+1);
-        printf("enter the roll no.: ");
-        scanf("%d", &std[i].rollNo);
-        printf("enter the name: ");
-        scanf("%s", std[i].name);
-        printf("enter the percentage.: ");
-        scanf("%f", &std[i].perc);
-        printf("\n");
+int compare(const void *a, const void *b) {
+    return ((struct Student *)a)->roll_no - ((struct Student *)b)->roll_no;
+}
+
+int main() {
+    struct Student students[5];
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Percentage: ");
+        scanf("%f", &students[i].percentage);
     }
 
-    for(int i=0; i<5; i++)
-    {
+    qsort(students, 5, sizeof(struct Student), compare);
+
+    printf("\nStudent Records (Sorted by Roll Number):\n");
+    printf("Roll No\tName\tPercentage\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d\t%s\t%.2f\n", students[i].roll_no, students[i].name, students[i].percentage);
+    }
     
-        int min=std[i].rollNo, idx=i;
-        for(int j=i; j<5; j++)
-        {
-            if(min>std[j].rollNo)
-            {
-               
-                min=std[j].rollNo;
-                idx=j;
-            }
-        }
-       
-        std[idx].rollNo=std[i].rollNo;
-        std[i].rollNo=min;
-        char temp[20];
-        strcpy(temp,std[idx].name);
-        strcpy(std[idx].name,std[i].name);
-        strcpy(std[i].name,temp);
-        float num=std[idx].perc;
-        std[idx].perc=std[i].perc;
-        std[i].perc=num;
-    }
-
-    /*printing the array of structures*/
-    printf("\n\n\n\n");
-    for(int i=0; i<5; i++)
-    {
-        printf("the details of %d th student\n", i+1);
-        printf("the roll no.: ");
-        printf("%d\n", std[i].rollNo);
-        printf("the name: ");
-        printf("%s\n", std[i].name);
-        printf("enter the percentage.: ");
-        printf("%f\n", std[i].perc);
-        printf("\n");
-    }
+    return 0;
 }
