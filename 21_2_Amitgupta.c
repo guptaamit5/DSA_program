9a2fb2cc9e6e58e8a1b250bf11b772f0a3c7e32a
guptaamit5 <141409978+guptaamit5@users.noreply.github.com>
2023-09-08 10:22:50 +0530

    Update 21_2_Amitgupta.c

--- 21_2_Amitgupta.c
+++ 21_2_Amitgupta.c
@@ -5,78 +5,32 @@ SE-IT (sem 3)
 2023-24
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-/*function that prints the pascal's triangle*/
-void pascal(int new_row[],int prev_row[], int n, int a)
+int main()
 {
-    if(a==n-1)
-    {
-        return;
-    }
-    for(int i=0; i<a; i++)
-    {
-        //the new row element is sum of 2 elements of previous row
-        new_row[i]=prev_row[i]+prev_row[i-1];
-    }
-    new_row[a]=1;
-    for(int i=0; i<n-a; i++)
-    {
-        //gives triangular look
-        printf(" ");
-    }
-    for(int i=0; i<=a; i++)
-    {
-        //prints elements
-        printf("%3d", new_row[i]);
-    }
-    printf("\n");
-    for(int i=0; i<n; i++)
-    {
-        prev_row[i]=new_row[i];
-    }
-    pascal(new_row, prev_row, n, a+1);
-}
+    int arr[50][50];
 
-/*function giving factorial of n*/
-int fact(int n)
-{
-    if(n==0)
-    {
-        return 1;
-    }
-    return n*fact(n-1);
-}
+    int i = 0;
+    int j = 0;
 
-/*gives combination of (n, r)*/
-int comb(int n, int r)
-{
-    return fact(n)/(fact(n-r)*fact(r));
-}
-void main()
-{
-    //Pascal triangle
-    //accepting the number of rows
-    int n;
-    printf("enter the value of number of rows: ");
+    int n = 0;
+
+    printf("Enter the number of lines: ");
     scanf("%d", &n);
 
-    /*recursive method*/
-    
-    int arr1[n], arr2[n];
-    pascal(arr1, arr2, n+1, 0);
-    
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - 1 - i; ++j)
+            printf(" ");
 
-    /*by using factorial and combinations (commented down)*/
-    
-    // for(int i=0; i<n; i++)
-    // {
-    //     for(int j=0; j<=n-i; j++){
-    //         printf(" ");
-    //     }
-    //     for(int j=0; j<=i; j++){
-    //         printf("%3d", comb(i, j));
-    //     }
-    //     printf("\n");
-    // }
+        for (j = 0; j <= i; ++j) {
+            if (j == 0 || j == i)
+                arr[i][j] = 1;
+            else
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
