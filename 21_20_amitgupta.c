da27fe8d396cc2d0851eae8c3d097052d75fe0df
guptaamit5 <141409978+guptaamit5@users.noreply.github.com>
2023-10-16 10:50:10 +0530

    Add files via upload

---
+++ 21_20_amitgupta.c
@@ -0,0 +1,149 @@
+/*Program for Priority Queue by array implimentation*/
+
+/*
+name = Amit gupta
+Roll No. = 21
+SE-IT (sem 3)
+2023-24
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+//stdlib for malloc and free
+
+//strcture of a node of LL
+struct node
+{
+    struct node *next;
+    int info;
+};
+
+//to create memory for a node
+struct node* getnode()
+{
+    return ((struct node*)malloc(sizeof(struct node)));
+}
+
+//to free a node
+void freenode(struct node* p)
+{
+    free(p);
+}
+
+//initializing the LL
+struct node *list = NULL;
+
+void insert_element(int); 
+void delete_element(int); 
+void check_priority(int); 
+void display_priorityqueue(); 
+
+void main() 
+{ 
+    int n, choice;
+    while (1) 
+    { 
+        printf("\n\n\n\n-----These are the menu panel-----");
+        printf("\n1. To insert element by priority "); 
+        printf("\n2. To delete element by priority "); 
+        printf("\n3. To display priority queue "); 
+        printf("\n4. To exit");  
+        printf("\n\n\nEnter your choice : ");    
+        scanf("%d", &choice);   
+        switch(choice) 
+        { 
+        case 1: 
+            printf("\nEnter element to insert : "); 
+            scanf("%d",&n); 
+            insert_element(n); 
+            break; 
+        case 2: 
+            printf("\nEnter element to delete : "); 
+            scanf("%d",&n); 
+            delete_element(n); 
+            break; 
+        case 3: 
+            display_priorityqueue(); 
+            break; 
+        case 4: 
+            exit(0); 
+        default: 
+            printf("\n Please enter valid choice"); 
+        } 
+    } 
+}
+
+void insert_element(int data) 
+{ 
+    if(list==NULL)
+    { 
+        struct node *newnode;
+        newnode=getnode();
+        newnode->info=data;
+        newnode->next=list;
+        list=newnode;
+    } 
+    else 
+        check_priority(data); 
+}  
+void check_priority(int data) 
+{ 
+    struct node *nn, *temp;
+    nn=getnode();
+    nn->info=data;
+    temp=list;
+
+    while((data >= temp->next->info)&&(temp!=NULL))
+    { 
+        temp=temp->next;
+    }
+    nn->next = temp->next;
+    temp->next=nn;
+}
+
+//to delete a data from LL
+void delete_element(int data)  
+{ 
+    if(list==NULL)
+    {
+        //if empty
+        printf("Empty LL");
+        return;
+    }
+
+    struct node *temp, *t2;
+    temp =list;
+    t2=list;
+
+    while(temp->next!=NULL)
+    {
+        if(data==temp->info)
+        {
+            t2->next=t2->next->next;
+            freenode(temp);
+            return;
+        }
+        t2 = temp;
+        temp= temp->next;
+    }
+    printf("\n%d element not found in queue", data); 
+} 
+
+//to display LL
+void display_priorityqueue() 
+{
+    if(list==NULL)
+    {
+        //if empty
+        printf("Empty LL");
+        return;
+    }
+    struct node *temp;
+    temp=list;
+    while(temp->next!=NULL)
+    {
+        printf("%d\n", temp->info);
+        temp=temp->next;
+    }
+    printf("%d", temp->info);
+}
---
+++ 21_21_amitgupta.c
@@ -0,0 +1,91 @@
+/*Program for Selection and bubble sort*/
+
+/*
+name = Amit gupta
+Roll No. = 21
+SE-IT (sem 3)
+2023-24
+*/
+
+#include <stdio.h> 
+
+/* to swap elements of arr at i and j */
+void swap(int* arr, int i, int j) 
+{ 
+	int temp = arr[i]; 
+	arr[i] = arr[j]; 
+	arr[j] = temp; 
+} 
+
+/* selection sorting function */
+void selectionSort(int arr[], int n) 
+{ 
+	int i, j, min_idx; 
+
+	// one by one move partition of unsorted subarray 
+	for (i = 0; i < n - 1; i++) { 
+		// finding the minimum element in unsorted array 
+		min_idx = i; 
+		for (j = i + 1; j < n; j++){
+			if (arr[j] < arr[min_idx]) {
+				min_idx = j; 
+			}
+		}
+
+		// swaping the found minimum element with the first one
+		swap(arr, min_idx, i); 
+	} 
+} 
+
+/* To display array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i = 0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("\n"); 
+} 
+
+/* bubble sorting function */
+void bubbleSort(int arr[], int n) 
+{ 
+	int i, j; 
+	for (i = 0; i < n - 1; i++){
+		// Last i elements are already 
+		// in place 
+		for (j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]){
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+} 
+
+int main() 
+{ 
+	// User defined arrray
+	int n;
+    printf("Enter the no of elements to be sorted: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for(int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+
+	/*
+	// Static array
+	int arr[] = { 64, 25, 12, 22, 11 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	*/
+
+	// for selection sort
+	// selectionSort(arr, n); 
+
+	// for bubble sort
+    bubbleSort(arr, n);
+
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+}
